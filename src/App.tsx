93ee8b44f86fb0065d6e11ace800c456c791276e
Harenabs21 <hei.harena.6@gmail.com>
2024-07-24 10:45:22 +0300

    feat: add jsonserver mock admin

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,15 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import jsonServerProvider from 'ra-data-json-server';
+import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin';
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
-  );
-}
+const App = () => (
+  <Admin title="patrimoine admin" dataProvider={dataProvider}>
+    <Resource name="users" edit={EditGuesser} show={ShowGuesser} list={ListGuesser} />
+    <Resource name="posts" list={ListGuesser} show={ShowGuesser} />
+    <Resource name="comments" edit={EditGuesser} show={ShowGuesser} list={ListGuesser} />
+  </Admin>
+);
 
 export default App;
