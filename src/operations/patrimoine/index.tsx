4c9a4b297dc1ff9c3919cc32db1d4c17cd8ca37c
Harenabs21 <hei.harena.6@gmail.com>
2024-07-26 10:52:19 +0300

    feat: wip: implement the components operation for patrimoine resource

---
+++ src/operations/patrimoine/index.tsx
@@ -0,0 +1,9 @@
+import { PatrimoineEdit } from './patrimoine-edit';
+import { PatrimoineList } from './patrimoine-list';
+import { PatrimoineShow } from './patrimoine-show';
+
+export const patrimoine_ui = {
+  list: <PatrimoineList />,
+  show: <PatrimoineShow />,
+  edit: <PatrimoineEdit />,
+};
---
+++ src/operations/patrimoine/patrimoine-show.tsx
@@ -0,0 +1,28 @@
+import { EditButton, Show, SimpleShowLayout, TopToolbar, TextField, DateField, FunctionField } from 'react-admin';
+import { useParams } from 'react-router-dom';
+import { renderMoneyWithAriary } from '../util';
+
+const PatrimoinShowAction = () => {
+  return (
+    <TopToolbar>
+      <EditButton />
+    </TopToolbar>
+  );
+};
+
+export const PatrimoineShow = () => {
+  const { id } = useParams();
+  return (
+    <Show id={id} actions={<PatrimoinShowAction />}>
+      <SimpleShowLayout>
+        <TextField source="nom" label={'Nom'} />
+        <TextField source="possesseur.nom" label={'Possesseur'} />
+        <DateField source="t" label={'Date'} />
+        <FunctionField
+          label={'Valeur comptable'}
+          render={(patrimoine) => renderMoneyWithAriary(patrimoine.valeur_comptable)}
+        />
+      </SimpleShowLayout>
+    </Show>
+  );
+};
