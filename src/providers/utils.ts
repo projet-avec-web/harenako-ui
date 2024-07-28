29de58ff7242a3f7e0990af2d64ee444dca621ae
Harenabs21 <hei.harena.6@gmail.com>
2024-07-28 23:41:22 +0300

    util: get PossessionTypeValue

--- src/providers/utils.ts
+++ src/providers/utils.ts
@@ -1,4 +1,19 @@
+import { PossessionAvecType } from '@harena-com/typescript-client';
+
 export const addIdentifier = <T>(data: T, key: keyof T): T & { id: T[keyof T] } => ({
   ...data,
   id: data[key],
 });
+
+export const getPossessionTypeValue = (value: PossessionAvecType) => {
+  switch (value.type) {
+    case 'ARGENT':
+      return value.argent!;
+    case 'FLUXARGENT':
+      return value.flux_argent!;
+    case 'MATERIEL':
+      return value.materiel!;
+    default:
+      throw new Error('Unknown Possession Type value');
+  }
+};
