da106f10cf71d23f1c1027448c1a87abf2252475
Harenabs21 <hei.harena.6@gmail.com>
2024-07-28 23:42:03 +0300

    chore: change unknown type to any

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,8 +1,8 @@
 export type MutationType = 'CREATE' | 'UPDATE';
 
 export type HarenakoProviderTemplate<T> = {
-  getOne: (name: string, meta: unknown) => Promise<T>;
-  getList: (page: number, pageSize: number, filter: unknown, sort: unknown, meta: unknown) => Promise<T[]>;
-  saveOrUpdate: (payload: T, meta: { mutationType: MutationType; [T: string]: unknown }) => Promise<T>;
-  delete: (name: string, meta: unknown) => Promise<T>;
+  getOne: (name: string, meta: any) => Promise<T>;
+  getList: (page: number, pageSize: number, filter: any, sort: any, meta: any) => Promise<T[]>;
+  saveOrUpdate: (payload: T, meta: { mutationType: MutationType; [T: string]: any }) => Promise<T>;
+  delete: (name: string, meta: any) => Promise<T>;
 };
