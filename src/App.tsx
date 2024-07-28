7dcf92232345ba1589c2d2e8df0104f0fbc3e6d2
Harenabs21 <hei.harena.6@gmail.com>
2024-07-28 16:12:14 +0300

    feat: create custom app-bar and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Admin, Resource } from 'react-admin';
 import { dataProvider } from './providers/data-provider';
 import { patrimoine_ui } from './operations/patrimoine';
+import { CustomLayout } from './components/layout/layout';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 
 const App = () => (
-  <Admin title="patrimoine admin" dataProvider={dataProvider}>
-    <Resource name="patrimoines" {...patrimoine_ui} />
+  <Admin title="patrimoine admin" layout={CustomLayout} dataProvider={dataProvider}>
+    <Resource icon={AccountBalanceIcon} name="patrimoines" {...patrimoine_ui} />
   </Admin>
 );
 
