083f6a2112932fd7b1cbe068746999947016f3d8
Harenabs21 <hei.harena.6@gmail.com>
2024-07-29 00:23:08 +0300

    chore: format code

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,4 +4,3 @@ import { getConfiguration } from './utils';
 export const patrimoineApi = () => new PatrimoineApi(getConfiguration());
 export const possessionsApi = () => new PossessionApi(getConfiguration());
 export const projectionFuturApi = () => new ProjectionFutureApi(getConfiguration());
-
